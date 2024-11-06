89ce411628839b74100724a69289b318aaaa987c
Sebdenmark <Sebdenmark@gmail.com>
2024-11-06 09:53:36 +0100

    objekt

--- dungoenescape/obj/Debug/net8.0/ConsoleApp1.AssemblyInfo.cs
+++ dungoenescape/obj/Debug/net8.0/ConsoleApp1.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+82ae8c1cae09eee84869303fba595f709a0401f1")]
 [assembly: System.Reflection.AssemblyProductAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
---
+++ opjekt dag 1-2/Program.cs
@@ -0,0 +1,34 @@
+﻿namespace opjekt_dag_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("put in your full name");
+
+            string fullname = Console.ReadLine();
+            //here we make an instance of the bankaccount 
+            Account myAccount = new Account(fullname);
+
+            Console.WriteLine($"Saldo: {myAccount.GetBalance()} kr");
+
+
+            Console.WriteLine("how much do you want to deposit");
+            decimal newDeposit = decimal.Parse( Console.ReadLine() );
+            
+            myAccount.Deposit(newDeposit);
+
+
+            Console.WriteLine("how muc do you want to withdraw");
+            decimal newwithdraw = decimal.Parse( Console.ReadLine() );
+   
+            myAccount.Withdraw(newwithdraw);
+
+    
+            Console.WriteLine($"{myAccount.GetName()} customer id: {myAccount.GetCustomerid()} Saldo: {myAccount.GetBalance()} kr");
+
+            Console.ReadLine();
+        }
+    }
+}
