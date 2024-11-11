3f520a81719324845bf81f5dc6e5df52c2a7371d
Sebdenmark <Sebdenmark@gmail.com>
2024-11-11 09:47:45 +0100

    øvelse 1

---
+++ dag 6 objekt/Program.cs
@@ -0,0 +1,44 @@
+﻿namespace dag_6_objekt
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int beløb = 0;
+            int konto = 100;
+
+            try
+            {
+                Console.WriteLine("write amount");
+                beløb = int.Parse(Console.ReadLine());
+                konto = konto + beløb;
+                Console.WriteLine("Hvor meget vil du hæve? Du har en startsaldo på 100 kr.");
+                int withdraw = int.Parse(Console.ReadLine());
+
+                if (withdraw > konto)
+                {
+                    throw new InvalidOperationException("Saldoen er for lav til at gennemføre hævningen.");
+                }
+
+                konto -= withdraw; 
+
+                Console.WriteLine($"Du har hævet {withdraw} kr. Ny saldo: {konto} kr.");
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine("Ugyldigt input. Indtast venligst et gyldigt tal.");
+            }
+            catch (InvalidOperationException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            finally
+            {
+                Console.WriteLine($"here is the final acount {konto}");
+                Console.ReadLine(); 
+            }
+
+
+        }
+    }
+}
